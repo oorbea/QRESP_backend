f9f1fa521198538bf1ff4b04c680e74492e3686e
Oriol Orbea <oriol.orbea@gmail.com>
2024-12-14 20:13:39 +0100

    update qr path

--- diagnosticAlgorithm.mjs
+++ diagnosticAlgorithm.mjs
@@ -1,1 +1,13 @@
-import axios from 'axios';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+const prompt = 'Explain how AI works';
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
